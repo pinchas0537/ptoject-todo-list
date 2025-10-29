a048c9b115323168dfd11ccc8a77b3386004c4b3
Pinchas Lifshitz <pnl8480537@gmail.com>
2025-10-29 11:48:42 +0200

    finsh main func

---
+++ main.py
@@ -0,0 +1,22 @@
+import game
+
+def main() -> None:
+    while True:
+        show = game.get_user_choice()
+        match show:
+            case "1":
+                task = input("Enter a task:")
+                game.add_task(game.todo_list,task)
+            case "2":
+                game.show_all_tasks(game.todo_list)
+            case "3":
+                print("זה עדיין בפיתוח")
+            case "4":
+                print("זה עדיין בפיתוח")
+            case "5":
+                break
+            case _:
+                print("הבקשה לא נכונה!")
+            
+if __name__ == "__main__":            
+    main()
